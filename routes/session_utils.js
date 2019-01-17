7afae12dc19a2859164d9c3e5968ee78db131267
Eric Graham <photon0f@protonmail.com>
2019-01-16 23:01:31 -0600

    Rename session_utils functions

--- routes/session_utils.js
+++ routes/session_utils.js
@@ -4,14 +4,14 @@ let hashmap = require("hashmap");
 // acct number | secret
 let sessions = new HashMap();
 
-module.exports.handleLoggedIn = function (req) {
+function handleLoggedIn (req) {
     logger.info("handleLoggedIn() called with parameter " + req);
-};
+}
 
-module.exports.hashPassword = function (password) {
+function hashPassword (password) {
     // TODO
     argon.hash(password).then(hash => {
         logger.info("Calling hashPassword(" + password + "): " + hashed);
     });
     return password;
-};
+}
