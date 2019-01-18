3820cb8d80d46ad2ea9ca7c69bd17b0ecf94d2c0
Eric Graham <photon0f@protonmail.com>
2019-01-18 14:28:34 -0600

    Add logInUser() and call in /login

--- routes/login.js
+++ routes/login.js
@@ -7,22 +7,33 @@ let sessionUtils = require('../src/session_utils.js');
 router.get('/', function (req, res, next) {
     logger.info("Logging in user, running isUserLoggedIn(): " + sessionUtils.isUserLoggedIn(req));
 
-    if(req.cookies.logged_in == "true"){
-        res.redirect('/admin')
+    if (sessionUtils.isUserLoggedIn(req)) {
+        res.redirect('/admin');
     } else {
         res.render('login.html', {settings: settings})
     }
+    /*if(req.cookies.logged_in == "true"){
+        res.redirect('/admin')
+    } else {
+        res.render('login.html', {settings: settings})
+    }*/
 });
 router.post('/', function (req,res,next) {
    //TODO Add database checks
     aerospike.getUser(req.body.uname, function (result) {
         if(result.bins.pass == req.body.pass){
-            res.cookie("logged_in", true);
-            req.cookies.logged_in = true;
-            res.redirect('/admin')
-        } else {
-            res.render('login.html', {settings: settings, failed: true})
+            if (!sessionUtils.logInUser(req, res)) {
+                global.logger.info("Error while logging in user's session; inputted username must be undefined");
+            } else {
+                // res.cookie("logged_in", true);
+                // req.cookies.logged_in = true;
+
+                res.redirect('/admin');
+                return;
+            }
         }
+
+        res.render('login.html', {settings: settings, failed: true})
     });
 
 });
--- src/session_utils.js
+++ src/session_utils.js
@@ -5,10 +5,23 @@ let hm = require("hashmap");
 let sessionMap = new hm.HashMap();
 
 module.exports = {
+    // returns true if the user is logged in according to session cookies
     isUserLoggedIn(req) {
         let username = req.cookies.username, secret = req.cookies.secret;
-        global.logger.info("isUserLoggedIn() called.");
         return username !== undefined && secret !== undefined && isValidInSessionMap(username, secret)
+    },
+    // sets cookies and adds user to the session map
+    logInUser(req, res) {
+        let username = req.cookies.username;
+        if (username === undefined) return false;
+
+        argon2.generateSalt().then(salt => {
+            global.logger.info("Setting session for user " + username);
+            sessionMap.set(username, salt);
+            res.cookie("secret", salt, { maxAge: 1000 * 60 * 10 /* 10 minutes */, httpOnly: true })
+        });
+
+        return true;
     },
     hashPassword(password, salt = "") {
         // TODO
@@ -20,5 +33,13 @@ module.exports = {
 };
 
 function isValidInSessionMap(username, secret) {
-    return sessionMap.has(username) && sessionMap.get(username) === secret;
+    if (sessionMap.has(username)) {
+        if (sessionMap.get(username) !== secret) {
+            // Delete the user from the session list to prevent attackers from stealing the session
+            sessionMap.delete(username);
+            return false;
+        }
+        return true;
+    }
+    return false;
 }
