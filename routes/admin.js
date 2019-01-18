4647fadd7700a8c3da555d4ac06fc957423c9bb1
Eric Graham <photon0f@protonmail.com>
2019-01-18 15:22:24 -0600

    Check sessions in admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -1,27 +1,48 @@
 var express = require('express');
 var router = express.Router();
 let as = require('../src/aerospike');
+let sessionUtils = require('../src/session_utils.js');
 
 
 router.get('/', function (req,res,next) {
-    if(req.cookies.logged_in != "true"){
-        res.redirect('/login')
+    if(!sessionUtils.isUserLoggedIn(req)){
+        res.redirect('/login');
+        return;
     }
-    else {
-        res.render("admin.html", {settings: settings});
-    }
+
+    res.render("admin.html", {settings: settings});
 });
 router.get('/newAccount', function (req, res, next) {
+    if(!sessionUtils.isUserLoggedIn(req)){
+        res.redirect('/login');
+        return;
+    }
+
     res.render('newAccount.html', {settings: settings})
 });
 router.post('/newAccount', function (req, res, next) {
+    if(!sessionUtils.isUserLoggedIn(req)){
+        res.redirect('/login');
+        return;
+    }
+
     as.newAccount(req.body.account_number, req.body.owner, req.body.bal, req.body.pin);
     res.redirect("/account/num/" + req.body.account_number.toString())
 });
 router.get('/add', function (req, res, next) {
+    if(!sessionUtils.isUserLoggedIn(req)){
+        res.redirect('/login');
+        return;
+    }
+
     res.render('add.html',{settings: settings})
 });
 router.post('/add', function (req, res, next) {
+    if(!sessionUtils.isUserLoggedIn(req)){
+        res.redirect('/login');
+        return;
+    }
+
     as.addTransaction("add", req.body, function (err, result) {
         if (err){throw err}
         else {
@@ -30,9 +51,19 @@ router.post('/add', function (req, res, next) {
     })
 });
 router.get('/transfer', function (req, res, next) {
+    if(!sessionUtils.isUserLoggedIn(req)){
+        res.redirect('/login');
+        return;
+    }
+
     res.render('transfer.html', {settings: settings})
 });
 router.post('/transfer', function (req, res, next) {
+    if(!sessionUtils.isUserLoggedIn(req)){
+        res.redirect('/login');
+        return;
+    }
+
     req.body.destination={account_number: req.body.dacct, branch: req.body.dbranch};
     as.addTransaction("transfer", req.body, function (err, result) {
         if (err){throw err}
