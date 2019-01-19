2e8762ede23876a844daec492d527100ac70f9ee
Eric Graham <photon0f@protonmail.com>
2019-01-19 00:55:40 -0600

    Move logic in /login post

--- routes/login.js
+++ routes/login.js
@@ -6,7 +6,7 @@ let sessionUtils = require('../src/session_utils.js');
 
 router.get('/', function (req, res, next) {
     if (sessionUtils.isUserLoggedIn(req.cookies.username, req.cookies.secret)) {
-        global.logger.info("/login get: User logged in");
+        global.logger.info("/login get: User already logged in");
         res.redirect('/admin');
     } else {
         global.logger.info("/login get: User not logged in");
@@ -19,13 +19,13 @@ router.get('/', function (req, res, next) {
     }*/
 });
 router.post('/', function (req,res,next) {
-    if (req.body.uname !== undefined && req.body.pass !== undefined) {
-        if (sessionUtils.isUserLoggedIn(req.cookies.username, req.cookies.secret)) {
-            global.logger.info("/login post: user already logged in");
-            res.redirect('/admin');
-            return;
-        }
+    if (sessionUtils.isUserLoggedIn(req.cookies.username, req.cookies.secret)) {
+        global.logger.info("/login post: user already logged in");
+        res.redirect('/admin');
+        return;
+    }
 
+    if (req.body.uname !== undefined && req.body.pass !== undefined) {
         //TODO Add database checks
         aerospike.getUser(req.body.uname, function (result) {
             if(result.bins.pass === req.body.pass){
