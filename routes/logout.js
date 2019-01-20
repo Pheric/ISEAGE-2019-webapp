541abebe30c652d6af78cf47b6f22794229ed01a
Eric Graham <photon0f@protonmail.com>
2019-01-19 23:55:15 -0600

    Add one line that claimed an hour of my life

--- routes/logout.js
+++ routes/logout.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
-let sessionUtils = require("../src/session_utils.js");
+let sessionUtils = require('../src/session_utils.js');
 
 router.use('/', function (req,res,next) {
     sessionUtils.logoutUser(res);
     res.redirect('/');
 });
+
+module.exports = router;
