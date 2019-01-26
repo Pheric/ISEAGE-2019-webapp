570f89b44396b528f8eac88e8f8a4ab4e61d2ac4
Eric Graham <photon0f@protonmail.com>
2019-01-26 16:13:28 -0600

    Allow /login to fail corretly with nonexistent accounts

--- routes/login.js
+++ routes/login.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 let aerospike = require('../src/aerospike');
 let sessionUtils = require('../src/session_utils.js');
+let aero = require('aerospike');
 
 
 router.get('/', function (req, res, next) {
@@ -24,7 +25,11 @@ router.post('/', function (req,res,next) {
 
     if (req.body.uname !== undefined && req.body.pass !== undefined) {
         //TODO Add database checks
-        aerospike.getUser(req.body.uname, function (result) {
+        aerospike.getUser(req.body.uname, function (err, result) {
+            if (aero.status.AEROSPIKE_ERR_RECORD_NOT_FOUND == err.code) {
+                res.render('login.html', {settings: settings, failed: true})
+                return;
+            }
             if(sessionUtils.checkLogin(req.body.uname, req.body.pass, result.bins.pass, result.bins.salt)){ // result.bins.pass === req.body.pass
                 sessionUtils.logInUser(req.body.uname, res);
 
