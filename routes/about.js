a0ca2b5214cef6a0e8c1ac867122e67e2e8bc112
Eric Graham <photon0f@protonmail.com>
2019-01-25 22:19:52 -0600

    Style comments the actual way

--- routes/about.js
+++ routes/about.js
@@ -5,22 +5,19 @@ let as = require('../src/aerospike');
 
 router.post('/', function (req, res, next) {
     as.addComment(function () {
-res.redirect('/about/'+req.body.set)
+        res.redirect('/about/'+req.body.set)
     }, req.body.set || "comments", req.body)
 });
 
 router.get('/:set?', function (req, res, next) {
     if (req.params.set){
         as.getComments(function (err, all) {
-            for(i of all){
-                delete i.key.digest;
-                delete i.ttl;
-                delete i.gen;
-            }
-            let comments=JSON.stringify(all, null, 4).replace(/\\n/g, '\<br \/\>')
-            res.render('about.html', {settings: settings, comments:comments})
+            let comments = new Map();
+            for(i of all)
+                comments.set(i.bins.uname, i.bins.comment);
+            res.render('about.html', {settings: settings, comments: comments})
         }, req.params.set || "comments")
-    } else{
+    } else {
         as.getComments(function (err, all) {
             for(i of all){
                 delete i.key.digest;
